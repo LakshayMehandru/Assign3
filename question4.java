00b35fc377c474a56e5bcedd5b5b26d60f7ef5bd
Manpreet Singh <manpreetazimal@gmail.com>
2018-08-21 23:03:40 +0530

    WIP

---
+++ question4.java
@@ -0,0 +1,19 @@
+class Fourth
+{
+public static void main(String[] args)
+{
+int a=3,b=4,c=5;
+if(a>b&&a>c)
+{
+System.out.println("A IS GREATEST OF ALL");
+}
+else if(b>a&&b>c)
+{
+System.out.println("B IS GREATEST OF ALL");
+}
+else if(c>a&&c>b)
+{
+System.out.println("C IS GREATEST OF ALL");
+}
+}
+}
